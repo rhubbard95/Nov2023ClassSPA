7b0bab71866db719aae9df8217bc2130ba3dac62
Rashad Hubbard <shad.hubbard90@gmail.com>
2024-01-03 20:53:05 -0600

    9.2 lessons

--- index.js
+++ index.js
@@ -8,20 +8,77 @@ const router = new Navigo("/");
 
 function render(state = store.Home) {
   document.querySelector("#root").innerHTML = `
-  ${Header(state)}
-  ${Nav(store.Links)}
-  ${Main(state)}
-  ${Footer()}
+    ${Header(state)}
+    ${Nav(store.Links, state)}
+    ${Main(state)}
+    ${Footer()}
   `;
+
   router.updatePageLinks();
-  afterRender();
+  afterRender(state);
 }
 
-function afterRender() {
-  // add menu toggle to bars icon in nav bar//
+function afterRender(state) {
+  // add menu toggle to bars icon in nav bar
   document.querySelector(".fa-bars").addEventListener("click", () => {
     document.querySelector("nav > ul").classList.toggle("hidden--mobile");
   });
+
+  if (state.view === "Home") {
+    // Do this stuff
+    document.getElementById("callToAction").addEventListener("click", event => {
+      event.preventDefault();
+
+      router.navigate("/pizza");
+    });
+  }
+
+  if (state.view === "Order") {
+    // Add an event handler for the submit button on the form
+    document.querySelector("form").addEventListener("submit", event => {
+      event.preventDefault();
+
+      // Get the form element
+      const inputList = event.target.elements;
+      console.log("Input Element List", inputList);
+
+      // Create an empty array to hold the toppings
+      const toppings = [];
+
+      // Iterate over the toppings array
+
+      for (let input of inputList.toppings) {
+        // If the value of the checked attribute is true then add the value to the toppings array
+        if (input.checked) {
+          toppings.push(input.value);
+        }
+      }
+
+      // Create a request body object to send to the API
+      const requestData = {
+        customer: inputList.customer.value,
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings
+      };
+      // Log the request body to the console
+      console.log("request Body", requestData);
+
+      axios
+        // Make a POST request to the API to create a new pizza
+        .post(`${process.env.PIZZA_PLACE_API_URL}/pizzas`, requestData)
+        .then(response => {
+          //  Then push the new pizza onto the Pizza state pizzas attribute, so it can be displayed in the pizza list
+          store.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        // If there is an error log it to the console
+        .catch(error => {
+          console.log("It puked", error);
+        });
+    });
+  }
 }
 
 router.hooks({
@@ -33,11 +90,12 @@ router.hooks({
         : "Home";
     // Add a switch case statement to handle multiple routes
     switch (view) {
+      // New Case for the Home View
       case "Home":
         axios
           // Get request to retrieve the current weather data using the API key and providing a city name
           .get(
-            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=chicago`
           )
           .then(response => {
             // Convert Kelvin to Fahrenheit since OpenWeatherMap does provide otherwise
@@ -54,11 +112,11 @@ router.hooks({
 
             // An alternate method would be to store the values independently
             /*
-      store.Home.weather.city = response.data.name;
-      store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
-      store.Home.weather.feelsLike = kelvinToFahrenheit(response.data.main.feels_like);
-      store.Home.weather.description = response.data.weather[0].main;
-      */
+            store.Home.weather.city = response.data.name;
+            store.Home.weather.temp = kelvinToFahrenheit(response.data.main.temp);
+            store.Home.weather.feelsLike = kelvinToFahrenheit(response.data.main.feels_like);
+            store.Home.weather.description = response.data.weather[0].main;
+            */
             done();
           })
           .catch(err => {
@@ -66,21 +124,37 @@ router.hooks({
             done();
           });
         break;
+
       // Add a case for each view that needs data from an API
       case "Pizza":
         // New Axios get request utilizing already made environment variable
         axios
           .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
           .then(response => {
-            // We need to store the response to the state, in the next step but in the meantime let's see what it looks like so that we know what to store from the response.
+            // We need to store the response to the state, in the next step but in the meantime
+            //   let's see what it looks like so that we know what to store from the response.
             console.log("response", response.data);
             store.Pizza.pizzas = response.data;
+
             done();
           })
           .catch(error => {
             console.log("It puked", error);
             done();
           });
+        break;
+      case "Products":
+        axios.get("https://fakestoreapi.com/products").then(response => {
+          // store.Products.products = response.data.map(product => {
+          //   return {
+          //     title: product.title + " matsinet",
+          //     image: product.image
+          //   };
+          // });
+          store.Products.products = response.data;
+
+          done();
+        });
         break;
       default:
         done();
@@ -110,10 +184,3 @@ router
     }
   })
   .resolve();
-
-// render();
-
-// add menu toggle to bars icon in nav bar//
-//document.querySelector(".fa-bars").addEventListener("click", () => {
-//  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-//});
